37543133d9660634942dae0fe0a3c4f61c83bcc9
pradeepa <pradeepapaulraj31@gmail.com>
2026-03-10 16:55:39 +0530

    Add arrpractise.js

---
+++ arrpractise.js
@@ -0,0 +1,10 @@
+let status=["come","notcome","go","notgo","sleep","notsleep"];
+console.log(status.at(-1));
+console.log(status.at(-2));
+console.log(status.at(-3));
+console.log(status.at(-4));
+console.log(status.at(-5));
+console.log(status.at(-6))
+console.log(status.length);
+status[1]="blue"
+console.log(status);
