b43b3786672531bd658169c128dcfd35a13fe463
pradeepa <pradeepapaulraj31@gmail.com>
2026-03-11 11:38:13 +0530

    Update arrpractise.js

--- arrpractise.js
+++ arrpractise.js
@@ -1,10 +1,9 @@
-let status=["come","notcome","go","notgo","sleep","notsleep"];
-console.log(status.at(-1));
-console.log(status.at(-2));
-console.log(status.at(-3));
-console.log(status.at(-4));
-console.log(status.at(-5));
-console.log(status.at(-6))
-console.log(status.length);
-status[1]="blue"
-console.log(status);
+let fruit=[[75,65,],[12,32]];
+let result=(fruit.flat());
+console.log(result);
+
+
+
+
+
+
